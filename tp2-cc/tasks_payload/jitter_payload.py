afcc8724861437dcff18e5bb38dc244e583574b4
Rodrigo Granja <115477645+rgranja17@users.noreply.github.com>
2024-11-25 13:49:15 +0000

    Delete tp2-cc directory

--- tp2-cc/tasks_payload/jitter_payload.py
+++
@@ -1,7 +0,0 @@
-from .bandwidth_payload import BandwidthPayload
-
-class JitterPayload(BandwidthPayload):
-    def __init__(self,mode,server_ip,transport,duration,threshold_value):
-        super().__init__(mode, server_ip,transport,duration,threshold_value)
-      
-        
